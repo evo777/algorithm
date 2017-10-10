03a49a66957b0e1c3a2f4c99acc2253c79d54305
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-10-09 21:13:48 -0700

    Reactored code to one line

--- solutions/6.js
+++ solutions/6.js
@@ -1,7 +1,5 @@
 const callX2 = (func, n, i = 1) => {
-  if (i === n) return func(n);
-  func(i);
-  return callX2(func, n, i + 1);
+  if (i < n + 1) func(i) + callX2(func, n, i + 1);
 };
 
 module.exports = callX2;
