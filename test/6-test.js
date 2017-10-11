b68ab0a05274558fdbe201fdd62c21badb690a92
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-10-10 19:17:03 -0700

    Fixed test

--- test/6-test.js
+++ test/6-test.js
@@ -1,9 +1,16 @@
 const solution = require('../solutions/6.js');
 
-const f = (i) => {
-  console.log(i);
+const test = (num) => {
+  let count = 0;
+  const f = i => {
+    count++;
+  };
+
+  solution(test, num);
+
+  count === num ? console.log('Test passed') : console.log('Test failed');
 };
 
-solution(f, 5);
-solution(f, 6);
-solution(f, 1);
+test(f, 5);
+test(f, 6);
+test(f, 1);
