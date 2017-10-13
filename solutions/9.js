918802d4244e8ae9b17454a0805de2e5a7b564f5
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-10-12 20:49:28 -0700

    Complete 9

---
+++ solutions/9.js
@@ -0,0 +1,12 @@
+/*
+ 
+ Write a function that finds the greatest common denominator
+ 
+ */
+
+const gcd = (num1, num2, i = 1; result=1) => {
+  const min = Math.min(num1, num2);
+  if (i === min) return result;
+  if (num1 % i === 0 && num2 % i === 0 && result <= i) result = i;
+  return gcd(num1, num2, i+1, result);
+};
